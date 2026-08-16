0a33772e3a6c47315e50a4fe6ea7db44cff26a4b
DillonCordiner <dillon.cordiner@gmail.com>
2026-08-16 14:31:00 -0400

    0.0.6 small hotkey fix

--- ReplayTestMod/ReplayTestMod/InputListener.cs
+++ ReplayTestMod/ReplayTestMod/InputListener.cs
@@ -52,7 +52,7 @@ namespace ReplayTestMod
 
             if ((currentState is ReplayState) || (currentState is PlayState))
             {
-                if (Input.GetKeyDown(Main.settings.noiseHotkey.keyCode))
+                if ((!Input.GetKey(KeyCode.LeftControl) | !Input.GetKey(KeyCode.RightControl)) && Input.GetKeyDown(Main.settings.noiseHotkey.keyCode))
                 {
                     Main.camNoiseController.ToggleNoise();
                 }
